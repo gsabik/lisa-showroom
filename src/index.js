ef754e8e3c3e2ba82092e1bf16006b05d1d91473
Guido Sabik <guido.sabik@gmail.com>
2022-05-05 22:59:58 -0300

    clean code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
 import { ChakraProvider } from "@chakra-ui/react"
-import reportWebVitals from "./reportWebVitals";
 import App from "./App";
 import "./index.css";
 
@@ -13,8 +12,3 @@ root.render(
       <App/>
     </ChakraProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
