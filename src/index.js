f57bd9532ed370b07633eb5c43d26e77d0c38415
Guido Sabik <guido.sabik@gmail.com>
2022-08-16 20:53:00 -0300

    delete index.css file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { createRoot } from "react-dom/client"
 import { ChakraProvider } from "@chakra-ui/react"
 import App from "./App";
-import "./index.css";
 
 const divRoot = document.getElementById("root");
 const root = createRoot(divRoot);
